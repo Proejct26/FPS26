04f73c3e428136f73f23211d8ebad345093f1ed2
N0name4-SDH <soh0927@naver.com>
2025-03-27 19:09:57 +0900

    [Feat] 서버에 보낼 데이터 클래스
    간단하게나마 서버에 보낼 데이터 클래스를 만들어 보았습니다.

---
+++ Assets/Script/2. Handler/Player/PlayerStatData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PlayerStateData
+{
+    public Vector3 position;
+    public Vector2 moveInput;
+    public Vector2 lookInput;
+    public bool isJumping;
+    public bool isFiring;
+    public bool hitSuccess;
+    public string hitTargetId;
+}

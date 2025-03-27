81ff7ab5181e5502b363d311e3e1d08391c6d342
N0name4-SDH <soh0927@naver.com>
2025-03-27 16:36:00 +0900

    [Feat] 플레이어 스탯 담당 PlayerStatHandler 작성
    플레이어의 체력, 움직임 속도, 점프력, 공격력, 방어력 설정

---
+++ Assets/Script/2. Handler/PlayerStatHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatHandler : MonoBehaviour
+{
+    public float MaxHealth { get; set; }
+    public float MoveSpeed { get; set; }
+    public float JumpPower { get; set; }
+    public float AttackPower { get; set; }
+    public float DefensePower { get; set; }
+    public float CurrentHealth { get; set; }
+
+    private void Awake()
+    {
+
+    }
+
+    /// <summary>
+    /// 아이템 사용, 장비 시 스텟 변동을 위해 처리
+    /// </summary>
+    /// <param name="health"></param>
+    /// <param name="speed"></param>
+    /// <param name="jump"></param>
+    /// <param name="attack"></param>
+    public void SetModifier(float? health = null, float? speed = null, float? jump = null, float? attack = null, float? defense = null)
+    {
+        if (health.HasValue) MaxHealth = health.Value;
+        if (speed.HasValue) MoveSpeed = speed.Value;
+        if (jump.HasValue) JumpPower = jump.Value;
+        if (attack.HasValue) AttackPower = attack.Value;
+        if (defense.HasValue) DefensePower = defense.Value;
+    }
+
+    /// <summary>
+    /// 총알에 피격 당했을 시
+    /// </summary>
+    /// <param name="amount"></param>
+    public void TakeDamage(float amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0f)
+        {
+            CurrentHealth = 0f;
+            HandleDeath();
+        }
+    }
+
+    /// <summary>
+    /// 회복 아이템 획득 시
+    /// </summary>
+    /// <param name="amount"></param>
+    public void Heal(float amount)
+    {
+        CurrentHealth = Mathf.Min(CurrentHealth + amount, MaxHealth);
+    }
+
+    /// <summary>
+    /// 사망 시 처리
+    /// </summary>
+    private void HandleDeath()
+    {
+        Debug.Log("Player died.");
+    }
+
+}

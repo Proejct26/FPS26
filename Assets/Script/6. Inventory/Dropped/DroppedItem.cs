5bb04648a2b53357efe104d7def4b4f04ca42d59
ChinoDaiski <callmeson1@naver.com>
2025-03-27 15:10:37 +0900

    [Feat] : 인벤토리 UI
    이전에 만들었던 인벤토리 UI 추가

---
+++ Assets/Script/6. Inventory/Dropped/DroppedItem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DroppedItem
+{
+    public ItemData ItemData;
+
+    void Gather()
+    {
+        //TestManager.Instance.poolManager.Release(this);
+        //인벤토리로 들어가는 메서드
+    }
+}

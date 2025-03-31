88a26e9cc137edcd78f9e55bd3afb2217d54d4b9
subin <subin24890@gmail.com>
2025-03-31 11:41:34 +0900

    [Wip] 미니맵 작업 커밋

--- Assets/Script/5. UI/Scene UI/MiniMap.cs
+++ Assets/Script/5. UI/Scene UI/MiniMap.cs
@@ -9,16 +9,28 @@ public class MiniMap : UI_Scene
 
     public Image playerIcon;  // 미니맵에서 플레이어 아이콘
     public Image enemyIcon;
-    
+
     Transform playerTransform; // 플레이어 Transform (Managers.Player 사용 가능)
-    //Transform enemyTransform;
+    Transform enemyTransform; //적 위치 변수
 
     public RenderTexture miniMapTexture;//미니맵 창
+
+    Camera miniMapCam;//OnBecameVisible 사용 테스트
 
 
     void Start()
     {
         playerTransform = Managers.Player.gameObject.GetComponent<Transform>();
+        enemyTransform = GameObject.FindWithTag("LocalPlayer")?.transform;
+
+        if (enemyTransform == null)
+        {
+            Debug.Log("없음");
+        }
+        else if (enemyTransform != null)
+        {
+            Debug.Log("있슴");
+        }
 
         OnMiniMapCamere();//미니맵 연동
     }
@@ -29,10 +41,19 @@ public class MiniMap : UI_Scene
         float playerRotation = playerTransform.eulerAngles.y;
         playerIcon.rectTransform.rotation = Quaternion.Euler(0, 0, -playerRotation);
 
-        /*적 아이콘 시야각도 연동
-        float enemyRotation = enemyTransform.eulerAngles.y;
-        enemyIcon.rectTransform.rotation = Quaternion.Euler(0, 0, -enemyRotation);
-        */
+        OnBecameVisible();
+
+        if (enemyTransform != null)
+        {
+            enemyIcon.rectTransform.position = enemyTransform.position;
+
+            // 적 아이콘 회전 (회전값 적용)
+            float enemyRotation = enemyTransform.eulerAngles.y;
+            enemyIcon.rectTransform.rotation = Quaternion.Euler(0, 0, -enemyRotation);
+
+        }
+
+        OnBecameInvisible();
     }
 
 
@@ -42,7 +63,8 @@ public class MiniMap : UI_Scene
     void OnMiniMapCamere()
     {
         GameObject miniMapCamera = new GameObject("MiniMapCam");//오브젝트 생성
-        Camera miniMapCam = miniMapCamera.AddComponent<Camera>();//컴포넌트 생성
+
+        miniMapCam = miniMapCamera.AddComponent<Camera>();//컴포넌트 생성
 
         miniMapCamera.transform.SetParent(Managers.Player.transform);//카메라 오브젝트 플레이어 자식에 위치변경
 
@@ -51,6 +73,7 @@ public class MiniMap : UI_Scene
 
         miniMapCam.targetTexture = miniMapTexture;//미니맵 창이랑 연동
         miniMapCam.cullingMask = ~(1 << 3) | (1 << 5);//플레이어 오브젝트와 무기 오브젝트 카메라에서 제외
+
     }
 
     /// <summary>
@@ -58,11 +81,18 @@ public class MiniMap : UI_Scene
     /// </summary>
     void OnBecameVisible()
     {
-        if (gameObject.CompareTag("Player"))
+        if (gameObject.CompareTag("LocalPlayer"))
         {
-            return; // 플레이어는 인식 없슴
+            enemyIcon.gameObject.SetActive(true);
         }
-
-
     }
+
+    void OnBecameInvisible()
+    {
+        if (gameObject.CompareTag("LocalPlayer"))
+        {
+            enemyIcon.gameObject.SetActive(false);
+        }
+    }
+
 }

23aa33d584d39583b82c970d83c933c04860aa9a
Udi <dnel0605@gmail.com>
2025-04-01 09:42:59 +0900

    [Fix] 무기 생성시 위치, 회전값 문제 수정
    무기 생성시 제대로된 위치, 회전값이 보장되지 않았던 문제 수정

--- Assets/Script/5. UI/UI_Popup.cs
+++ Assets/Script/5. UI/UI_Popup.cs
@@ -6,23 +6,21 @@ using System;
 
 public class UI_Popup : UI_Base
 {
-    public override void Init()
-    {
-       Managers.UI.SetCanvas(gameObject, true);
-    }
+
+   public override void Init()
+   {
+      Managers.UI.SetCanvas(gameObject, true);
+   }
 
 
    public virtual void ClosePopupUI()
    {
       Managers.UI.ClosePopupUI(this, 0.5f); 
+
       CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
         if (canvasGroup == null)
             canvasGroup = gameObject.GetOrAddComponent<CanvasGroup>(); 
 
       canvasGroup.DOFade(0, 0.2f).SetEase(Ease.InOutSine);
-       
    }
-
-
 } 
- 

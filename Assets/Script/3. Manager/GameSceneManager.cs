f9acd7d8bbb249df63c626a816b0736eb7732789
Udi <dnel0605@gmail.com>
2025-04-01 11:03:13 +0900

    [Feat] MainScene

---
+++ Assets/Script/3. Manager/GameSceneManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+
+[Serializable]
+public class SpawnData
+{
+    [SerializeField] private Transform _redTeamSpawnPointParent;
+    [SerializeField] private Transform _blueTeamSpawnPointParent; 
+
+    public List<Transform> RedTeamSpawnPoints {get; private set;} = new List<Transform>();
+    public List<Transform> BlueTeamSpawnPoints {get; private set;} = new List<Transform>();
+
+    private void Init()
+    {
+        foreach (Transform child in _redTeamSpawnPointParent)
+            RedTeamSpawnPoints.Add(child);
+        
+
+        foreach (Transform child in _blueTeamSpawnPointParent)
+            BlueTeamSpawnPoints.Add(child);
+    }
+
+    public Transform GetSpawnPosition(int team, int index = 0)
+    {
+        return team == 0 ? RedTeamSpawnPoints[index] : BlueTeamSpawnPoints[index];
+    } 
+}
+
+public class GameSceneManager : MonoBehaviour
+{
+    [SerializeField] private SpawnData _spawnData = new SpawnData();
+
+    public virtual void EnterScene()
+    {  
+        
+    }
+
+    public virtual void ExitScene()
+    {
+
+    } 
+
+    private void Start()
+    {
+        Managers.RegisterGameSceneManager(this); 
+    }
+
+    public Transform GetSpawnPosition(int team, int index = 0)
+    {
+        return _spawnData.GetSpawnPosition(team, index); 
+    } 
+ 
+}
